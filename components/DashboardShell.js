132605d313a1f2094304bfa6435876373993db6c
Oliver Cederborg <hi@olivercederborg.com>
2021-02-07 13:12:09 +0100

    Dashboard

--- components/DashboardShell.js
+++ components/DashboardShell.js
@@ -1,12 +1,22 @@
-import { useAuth } from "@/lib/auth";
-import AddCollectionModal from "@/components/AddCollectionModal";
+import Head from "next/head";
 import { MdLabel } from "react-icons/md";
 import useSWR from "swr";
+
+import Navigation from "@/components/Navigation";
+import { useAuth } from "@/lib/auth";
+import AddCollectionModal from "@/components/AddCollectionModal";
 import fetcher from "@/utils/fetcher";
 
 export default function DashboardShell({ children }) {
 	return (
 		<>
+			<Head>
+				<title>Dashboard - Tsks, just tasks.</title>
+
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<Navigation />
 			<main className='flex flex-col items-center min-h-screen text-white bg-gray-900'>
 				<section className='mt-14 container min-h-screen'>
 					<h1 className='text-4xl font-bold'>Collections</h1>
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,4 @@
-import Head from "next/head";
-
 import { useAuth } from "@/lib/auth";
-import Navigation from "@/components/Navigation";
 import AddCollectionModal from "@/components/AddCollectionModal";
 import DashboardShell from "@/components/DashboardShell";
 import EmptyState from "@/components/EmptyState";
@@ -9,26 +6,28 @@ import ShowCollections from "@/components/ShowCollections";
 import useSWR from "swr";
 import fetcher from "@/utils/fetcher";
 
-export default function Dashboard() {
+const Dashboard = () => {
 	const { user } = useAuth();
 	const { data } = useSWR(
 		user ? ["/api/collections", user.token] : null,
 		fetcher
 	);
 
-	return (
-		<>
-			<Head>
-				<title>Dashboard - Tsks, just tasks.</title>
+	if (!data) {
+		return (
+			<DashboardShell>
+				<h1>Loading</h1>
+			</DashboardShell>
+		);
+	}
 
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-
-			<Navigation />
-
+	if (data.collections.length) {
+		return (
 			<DashboardShell>
 				<ShowCollections collections={data?.collections} />
 			</DashboardShell>
-		</>
-	);
-}
+		);
+	}
+};
+
+export default Dashboard;
