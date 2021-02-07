c5ad3e1f0c8770b52fcfed4faa8229c692b5262b
Oliver Cederborg <hi@olivercederborg.com>
2021-02-07 15:17:32 +0100

    GetAllCollections temp fix

--- pages/api/collections.js
+++ pages/api/collections.js
@@ -1,9 +1,11 @@
 import { auth } from "@/lib/firebase-admin";
-import { getUserCollections } from "@/lib/db-admin";
+import { getUserCollections, getAllCollections } from "@/lib/db-admin";
 
-export default async (req, res) => {
-	const uid = req.headers.uid;
-	const { collections } = await getUserCollections(uid);
+export default async (_, res) => {
+	// console.log(req.headers);
+	// const uid = req.headers.uid;
+	// const { collections } = await getUserCollections(uid);
+	const { collections } = await getAllCollections();
 
 	return res.status(200).json({ collections });
 };
