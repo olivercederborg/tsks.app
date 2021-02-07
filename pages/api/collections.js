407c39f214b2c7b12f6582f5a8845b794d340582
Oliver Cederborg <hi@olivercederborg.com>
2021-02-07 12:57:06 +0100

    Get Collections.

--- pages/api/collections.js
+++ pages/api/collections.js
@@ -4,7 +4,7 @@ import { getUserCollections } from "@/lib/db-admin";
 export default async (req, res) => {
 	try {
 		const { user_id } = await auth.verifyIdToken(req.headers.token);
-		const { collections } = await getUserCollections(user_id);
+		const { collections } = await getUserCollections(user_id ?? null);
 
 		return res.status(200).json({ collections });
 	} catch (error) {
