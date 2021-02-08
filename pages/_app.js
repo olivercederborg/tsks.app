9022046dd11c76981b2ffc697b5ff2b6680be8a0
Oliver Cederborg <hi@olivercederborg.com>
2021-02-08 03:06:04 +0100

    Sass

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "tailwindcss/tailwind.css";
 import { AuthProvider } from "../lib/auth";
 import { ChakraProvider } from "@chakra-ui/react";
+import "@/styles/style.scss";
 
 function MyApp({ Component, pageProps }) {
 	return (
