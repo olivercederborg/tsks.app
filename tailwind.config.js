8d30641a60dd7fbd4aa46f882b9afca1205a145a
Oliver Cederborg <hi@olivercederborg.com>
2021-02-09 23:52:56 +0100

    Added purgeCSS to layouts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ module.exports = {
 	purge: {
 		content: [
 			"./pages/**/*.{js,ts,jsx,tsx}",
-			"./components/**/*.{js,ts,jsx,tsx}"
+			"./components/**/*.{js,ts,jsx,tsx}",
+			"./layouts/**/*.{js,ts,jsx,tsx}"
 		],
 		options: {
 			safelist: [
