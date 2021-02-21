d43a7512edce4323465c2ca3a1c9d090647c1fcf
Oliver Cederborg <3323050@rts-365.dk>
2021-02-21 18:24:03 +0100

    Account settings functionality.

--- components/UpdateUserPassword.js
+++ components/UpdateUserPassword.js
@@ -1,7 +1,6 @@
 import { useRef } from "react";
 import { useForm } from "react-hook-form";
 import firebase from "firebase/app";
-import { mutate } from "swr";
 import {
 	Button,
 	Modal,
@@ -19,65 +18,30 @@ import {
 } from "@chakra-ui/react";
 
 import { useAuth } from "@/lib/auth";
-import { updateDbEmail } from "@/lib/db";
-import { getUser } from "@/lib/db-admin";
 
-const UpdateUserEmail = ({ user }) => {
-	const { reauthUser, verifyEmail } = useAuth();
+const UpdateUserPassword = ({ user }) => {
+	const { reauthUser } = useAuth();
 	const initialRef = useRef(null);
 	const toast = useToast();
 	const { isOpen, onOpen, onClose } = useDisclosure();
 	const { handleSubmit, register } = useForm();
 
-	const updateUserEmail = ({ newEmail, currentPassword }) => {
+	const updateUserPassword = ({ newPassword, currentPassword }) => {
 		reauthUser(currentPassword)
 			.then(() => {
 				firebase
 					.auth()
-					.currentUser.updateEmail(newEmail)
+					.currentUser.updatePassword(newPassword)
 					.then(function () {
 						toast({
 							title: "Success!",
-							description:
-								"Your email was updated. Check your email to verify.",
+							description: "Your password was updated.",
 							status: "success",
 							position: "top",
-							duration: 10000,
+							duration: 5000,
 							isClosable: true
 						});
-						console.log("Email updated to", newEmail);
-						updateDbEmail(user.uid, newEmail);
-						setTimeout(() => {
-							mutate(["/api/userdata", user.uid], async () => {
-								return getUser(user.uid);
-							});
-						}, 500);
 						onClose();
-						verifyEmail();
-					})
-					.catch(function (error) {
-						if (error?.code == "auth/requires-recent-login") {
-							console.log("Error happened", error);
-							toast({
-								title: "Error",
-								description:
-									"You need to have logged in recently to change your email.",
-								status: "error",
-								position: "top",
-								duration: 10000,
-								isClosable: true
-							});
-						}
-						if (error?.code == "auth/email-already-in-use") {
-							toast({
-								title: "Error",
-								description: "That email is already in use.",
-								status: "error",
-								position: "top",
-								duration: 10000,
-								isClosable: true
-							});
-						}
 					});
 			})
 			.catch((error) => {
@@ -118,34 +82,21 @@ const UpdateUserEmail = ({ user }) => {
 				<ModalOverlay />
 				<ModalContent
 					as='form'
-					onSubmit={handleSubmit(updateUserEmail)}
+					onSubmit={handleSubmit(updateUserPassword)}
 					bgColor='#1D1D27'
 					borderRadius='20px'
 					color='white'
 					pt='2'
 					mx='5'
 				>
-					<ModalHeader fontWeight='700'>Update Email</ModalHeader>
+					<ModalHeader fontWeight='700'>Update Password</ModalHeader>
 					<ModalCloseButton />
 					<ModalBody pb={6}>
 						<FormControl>
-							<FormLabel>New email</FormLabel>
-							<Input
-								ref={initialRef}
-								placeholder='contact@janedoe.com'
-								border='3px solid'
-								borderColor='#343343'
-								py='5'
-								px='4'
-								borderRadius='12px'
-								name='newEmail'
-								id='newEmail'
-								mb='6'
-								_hover={{ borderColor: "#3D3C50" }}
-								ref={register({ required: true })}
-							/>
 							<FormLabel>Current Password</FormLabel>
 							<Input
+								ref={initialRef}
+								minLength='8'
 								type='password'
 								border='3px solid'
 								borderColor='#343343'
@@ -154,6 +105,21 @@ const UpdateUserEmail = ({ user }) => {
 								borderRadius='12px'
 								name='currentPassword'
 								id='currentPassword'
+								mb='6'
+								_hover={{ borderColor: "#3D3C50" }}
+								ref={register({ required: true })}
+							/>
+							<FormLabel>New password</FormLabel>
+							<Input
+								type='password'
+								minLength='8'
+								border='3px solid'
+								borderColor='#343343'
+								py='5'
+								px='4'
+								borderRadius='12px'
+								name='newPassword'
+								id='newPassword'
 								mb='6'
 								_hover={{ borderColor: "#3D3C50" }}
 								ref={register({ required: true })}
@@ -188,4 +154,4 @@ const UpdateUserEmail = ({ user }) => {
 	);
 };
 
-export default UpdateUserEmail;
+export default UpdateUserPassword;
--- pages/settings/account.js
+++ pages/settings/account.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Avatar, Skeleton } from "@chakra-ui/react";
+import { Avatar } from "@chakra-ui/react";
 import { IoChevronBackOutline } from "react-icons/io5";
 
 import Navigation from "@/layouts/Navigation";
@@ -10,6 +10,7 @@ import useSWR from "swr";
 import fetcher from "@/utils/fetcher";
 import Head from "next/head";
 import UpdateUserEmail from "@/components/UpdateUserEmail";
+import UpdateUserPassword from "@/components/UpdateUserPassword";
 
 const Account = () => {
 	const { user, signout, updateUserName, verifyEmail } = useAuth();
@@ -41,95 +42,101 @@ const Account = () => {
 						<CollectionDropdown />
 					</div>
 
-					<section className='px-6'>
-						<div className='flex items-center'>
-							<figure>
-								<Avatar w='80px' h='80px' src={user?.photoUrl} />
-							</figure>
-							<div className='ml-5'>
-								<p className='text-2xl font-semibold'>
-									{userData?.name ?? "No name set"}
-								</p>
-								<span className='user-role-gradient bg-primary-background text-xxs inline-block px-2 py-1 mt-2 font-semibold rounded-md'>
-									{user?.userRole?.toUpperCase()}
-								</span>
-							</div>
-						</div>
-
-						<article className='bg-primary-card rounded-xl px-5 py-6 mt-10'>
-							<section>
-								<p className='text-sm opacity-50'>Display Name</p>
-								<div className='flex items-center justify-between'>
-									<p className='font-medium'>
+					{user ? (
+						<section className=' px-6'>
+							<div className='flex items-center mb-10'>
+								<figure>
+									<Avatar w='80px' h='80px' src={user?.photoUrl} />
+								</figure>
+								<div className='ml-5'>
+									<p className='text-2xl font-semibold'>
 										{userData?.name ?? "No name set"}
 									</p>
-									<UpdateDisplayName
-										user={userData}
-										updateUserName={updateUserName}
-									/>
+									<span className='user-role-gradient bg-primary-background text-xxs inline-block px-2 py-1 mt-2 font-semibold rounded-md'>
+										{user?.userRole?.toUpperCase()}
+									</span>
 								</div>
-							</section>
+							</div>
 
-							<section className='mt-6'>
-								<p className='text-sm opacity-50'>Email</p>
-								<div className='flex items-center justify-between'>
-									<p className='font-medium'>
-										{userData?.email}{" "}
-										{user?.emailVerified ? (
-											<span className='ml-2 text-sm font-normal text-green-400'>
-												Verified
-											</span>
-										) : (
-											<a
-												onClick={verifyEmail}
-												className='opacity-70 ml-2 text-sm font-normal cursor-pointer'
-											>
-												Verify
-											</a>
-										)}
-									</p>
-									<UpdateUserEmail user={userData} />
-								</div>
-							</section>
+							{user?.provider == "password" && (
+								<article className='bg-primary-card rounded-xl px-5 py-6'>
+									<section>
+										<p className='text-sm opacity-50'>Display Name</p>
+										<div className='flex items-center justify-between'>
+											<p className='font-medium'>
+												{userData?.name ?? "No name set"}
+											</p>
+											<UpdateDisplayName
+												user={userData}
+												updateUserName={updateUserName}
+											/>
+										</div>
+									</section>
 
-							<section className='mt-6'>
-								<p className='text-sm opacity-50'>Password</p>
-								<div className='flex items-center justify-between'>
-									<p className='font-medium'>*********</p>
-									<button className='bg-ligther-gray-button px-4 py-2 text-sm font-medium rounded-md'>
-										Change
-									</button>
-								</div>
-							</section>
-						</article>
+									<section className='mt-6'>
+										<p className='text-sm opacity-50'>Email</p>
+										<div className='flex items-center justify-between'>
+											<p className='font-medium'>
+												{userData?.email}{" "}
+												{user?.emailVerified ? (
+													<span className='ml-2 text-sm font-normal text-green-400'>
+														Verified
+													</span>
+												) : (
+													<a
+														onClick={verifyEmail}
+														className='opacity-70 ml-2 text-sm font-normal cursor-pointer'
+													>
+														Verify
+													</a>
+												)}
+											</p>
+											{user?.emailVerified && (
+												<UpdateUserEmail user={userData} />
+											)}
+										</div>
+									</section>
 
-						<article className='bg-primary-card rounded-xl mt-5 overflow-hidden'>
-							<section className='px-5 py-6'>
-								<p className=' text-sm opacity-50'>Subscription</p>
-								<div className='flex items-center justify-between'>
-									<p className='font-medium'>Tsks Free</p>
-									<button className='bg-gradient-to-tr from-gradient1-button via-gradient2-button to-gradient3-button px-4 py-2 text-sm font-medium rounded-md'>
-										Upgrade to Pro
-									</button>
-								</div>
-							</section>
-							<a
-								href='#'
-								className='bg-hover-card block w-full py-4 font-medium text-center'
-							>
-								See the Pro Benefits
-							</a>
-						</article>
+									<section className='mt-6'>
+										<p className='text-sm opacity-50'>Password</p>
+										<div className='flex items-center justify-between'>
+											<p className='font-medium'>*********</p>
+											<UpdateUserPassword user={userData} />
+										</div>
+									</section>
+								</article>
+							)}
 
-						<div className='flex justify-center'>
-							<button
-								onClick={() => signout()}
-								className='bg-hover-card rounded-xl px-8 py-4 mt-5 font-medium'
-							>
-								Sign out
-							</button>
-						</div>
-					</section>
+							<article className='bg-primary-card rounded-xl mt-5 overflow-hidden'>
+								<section className='px-5 py-6'>
+									<p className=' text-sm opacity-50'>Subscription</p>
+									<div className='flex items-center justify-between'>
+										<p className='font-medium'>Tsks Free</p>
+										<button className='bg-gradient-to-tr from-gradient1-button via-gradient2-button to-gradient3-button hover:opacity-80 px-4 py-2 text-sm font-medium transition-all duration-200 ease-in-out rounded-md'>
+											Upgrade to Pro
+										</button>
+									</div>
+								</section>
+								<a
+									href='#'
+									className='bg-hover-card hover:bg-secondary-card block w-full py-4 font-medium text-center transition-all duration-200 ease-in-out'
+								>
+									See the Pro Benefits
+								</a>
+							</article>
+
+							<div className='flex justify-center'>
+								<button
+									onClick={() => signout()}
+									className='bg-hover-card hover:bg-secondary-card rounded-xl px-8 py-4 mt-5 font-medium transition-all duration-200 ease-in-out'
+								>
+									Sign out
+								</button>
+							</div>
+						</section>
+					) : (
+						""
+					)}
 				</section>
 			</Navigation>
 		</>
